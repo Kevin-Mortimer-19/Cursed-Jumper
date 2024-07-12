7159c0cd60df8c3dd6f78e2d01c82de626356567
Norapat Rerngkasetkig <papyrogaming@gmail.com>
2024-07-11 21:35:00 -0400

    Added shotgun shooting functionality

---
+++ src/entities/player/shotgun.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+@export var spread_amount_degrees: float
+@export var bullet_scene: PackedScene
+
+@export var fire_sound: AudioStream
+
+
+func shoot(amount: int) -> void:
+	SoundManager.play_sound_nonpositional(fire_sound)
+	# TODO: Play muzzle flash particle here
+	
+	var rng:= RandomNumberGenerator.new() 
+	for i in range(amount):
+		var bullet: Projectile = bullet_scene.instantiate()
+		add_child(bullet)
+		bullet.set_as_top_level(true)
+		(bullet as Node2D).global_transform = global_transform
+		bullet.velocity = (global_transform.x * ((1.0 + 0.3 * rng.randf()) * bullet.move_speed)) \
+				.rotated(deg_to_rad(rng.randf() * spread_amount_degrees))
+
+
---
+++ src/projectiles/projectile.gd
@@ -0,0 +1,41 @@
+class_name Projectile extends CharacterBody2D
+
+@export var damage: int
+@export var move_speed: float
+@export var lifetime: float
+
+@export_group("Sound")
+@export var enemy_impact: AudioStream
+@export var environment_impact: AudioStream
+
+@export_group("Node References")
+@export var hitbox: Area2D
+
+
+var _lifetime_remaining: float = 0.0
+
+
+func _ready() -> void:
+	hitbox.body_entered.connect(_on_body_entered)
+
+func _physics_process(delta: float) -> void:
+	_lifetime_remaining += delta
+	if _lifetime_remaining >= lifetime:
+		queue_free()
+	
+	move_and_slide()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is BaseEntity:
+		(body as BaseEntity).take_damage(damage)
+		if enemy_impact:
+			SoundManager.play_sound_from_position(enemy_impact, global_position)
+	else:
+		if environment_impact:
+			SoundManager.play_sound_from_position(environment_impact, global_position)
+	
+	# TODO: Play particle here
+	queue_free()
+
+
