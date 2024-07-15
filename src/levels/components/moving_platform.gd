a4c0e4b7fae197041186e77539f3bc69e7c2ab98
Norapat Rerngkasetkig <papyrogaming@gmail.com>
2024-07-15 18:28:40 -0400

    Platforms can be set to be one-time move

--- src/levels/components/moving_platform.gd
+++ src/levels/components/moving_platform.gd
@@ -1,5 +1,7 @@
 class_name MovingPlatform extends PathFollow2D
 
+## Is a one and done animation.
+@export var is_one_shot: bool = false
 ## How quickly the platform moves along the path.
 @export var move_speed: float
 ## Whether the platform is currently moving.
@@ -32,9 +34,12 @@ func _physics_process(delta: float) -> void:
 			if progress_ratio < 1.0:
 				progress_ratio += delta * move_speed
 			elif progress_ratio >= 1.0:
-				_is_reversing = true
-				if _wait_timer:
-					_wait_timer.start()
+				if not is_one_shot:
+					_is_reversing = true
+					if _wait_timer:
+						_wait_timer.start()
+				else:
+					set_physics_process(false)
 		else:
 			if progress_ratio > 0.0:
 				progress_ratio -= delta * move_speed
