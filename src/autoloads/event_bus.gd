4f70825ba65430f7243d53095dc0354f1644ea8d
Norapat Rerngkasetkig <papyrogaming@gmail.com>
2024-07-17 18:59:39 -0400

    Screenshake, hitfreeze support, improved camera

--- src/autoloads/event_bus.gd
+++ src/autoloads/event_bus.gd
@@ -1,9 +1,26 @@
 extends Node
 
+signal screenshake_requested(strength: float, decay_rate: float)
+signal hitfreeze_requested(timescale: float, duration: float)
+
+signal player_respawn_requested
+
 signal coin_created(global_position: Vector2)
 signal coin_picked_up(amount: int)
 signal coin_spent(amount: int)
 
+signal transition_finished
+
 var faster_enemy_curse_active: bool = false
+
+
+func _ready() -> void:
+	hitfreeze_requested.connect(_hitfreeze)
+
+func _hitfreeze(timescale: float, duration: float) -> void:
+	Engine.time_scale = timescale
+	await get_tree().create_timer(duration, true, false, true).timeout
+	Engine.time_scale = 1.0
+
 
 
