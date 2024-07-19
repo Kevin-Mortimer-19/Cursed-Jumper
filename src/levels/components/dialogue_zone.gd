c67e1957690798563c1c2c4d6e83acf016c7f2a6
Kevin-Mortimer-19 <70980411+Kevin-Mortimer-19@users.noreply.github.com>
2024-07-18 20:09:32 -0400

    added most dialogue zones + NPCs
    added all dialogue and related scenes that had yet to be implemented, except for those reliant on logic from the opening scene

--- src/levels/components/dialogue_zone.gd
+++ src/levels/components/dialogue_zone.gd
@@ -8,7 +8,7 @@ func _ready():
 	body_entered.connect(_on_activate)
 
 
-func _on_activate(_body: PhysicsBody2D):
+func _on_activate(_body: CharacterBody2D):
 	DialogueManager.show_example_dialogue_balloon(dialogue, first_line)
 	get_tree().paused = true
 	queue_free()
