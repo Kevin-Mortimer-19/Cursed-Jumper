6448c50c881c7e8f658b9e1f9e210f6a9e5b7dc1
Norapat Rerngkasetkig <papyrogaming@gmail.com>
2024-07-20 19:32:19 -0400

    Added coin counts to shuffle menu

--- src/menus/curse_lock_button.gd
+++ src/menus/curse_lock_button.gd
@@ -3,7 +3,7 @@ class_name LockButton extends Button
 signal lock_curse(index: int)
 signal unlock_curse(index: int)
 
-var unlocked_text: String = "LOCK"
+var unlocked_text: String = "LOCK (%s)"
 var locked_text: String = "UNLOCK"
 
 @export var index: int
@@ -14,6 +14,8 @@ var lock_price = 5
 
 
 func _ready() -> void:
+	unlocked_text = unlocked_text % lock_price
+	text = unlocked_text
 	mouse_entered.connect(SoundManager.play_ui_sound.bind(SoundManager.SOUND_BUTTON_HOVER))
 	focus_entered.connect(SoundManager.play_ui_sound.bind(SoundManager.SOUND_BUTTON_HOVER))
 	pressed.connect(SoundManager.play_ui_sound.bind(SoundManager.SOUND_BUTTON_CONFIRM))
